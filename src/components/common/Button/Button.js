b013271863b4c529488658a8d4e08db4b75282b8
Natalia <smolutka15@gmail.com>
2021-02-10 16:28:48 +0100

    Change hover display for buttons in ProductBox. Update price background on hover

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './Button.module.scss';
-
 const Button = ({ children, variant, noHover, className: propClassName, ...props }) => {
   const classes = [];
 
