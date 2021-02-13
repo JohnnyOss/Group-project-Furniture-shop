1bf93849699031a86e87a980cf1ec88cad9ec508
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-02-13 01:22:33 +0100

    Footer part2

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -100,11 +100,11 @@ const Footer = ({ children }) => (
     <div className={styles.bottomBar}>
       <div className='container'>
         <div className='row align-items-center'>
-          <div className='col-4 text-left'></div>
-          <div className={'col-4 text-center ' + styles.copyright}>
+          <div className='col-sm-12 text-center'></div>
+          <div className={'col-6 text-center ' + styles.copyright}>
             <p>©Copyright 2016 Bazar | All Rights Reserved</p>
           </div>
-          <div className={'col-4 text-right ' + styles.socialMedia}>
+          <div className={'col-6 text-right ' + styles.socialMedia}>
             <ul>
               <li>
                 <a href='/#'>
