ff2969425470ad59e428d6e4455d91774e8b146a
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-02-18 11:37:22 +0100

    Add NewFurniture to ProductPage

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
 import ProductMore from '../../common/ProductMore/ProductMore';
+import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 
 const ProductPage = () => {
   return (
     <div className={styles.root}>
       This is ProductPage
       <ProductMore />
+      <NewFurniture />
     </div>
   );
 };
