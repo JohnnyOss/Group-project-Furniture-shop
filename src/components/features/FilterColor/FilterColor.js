7922826b8bab9a6963433b91846f3f82539b55c8
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-02-22 13:42:30 +0100

    Add filtering by color

---
+++ src/components/features/FilterColor/FilterColor.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './FilterColor.module.scss';
+
+const FilterColor = () => {
+  return (
+    <div className={styles.filterHeading}>
+      <h6 className={styles.filterTitle}>{'FILTER BY COLOR'}</h6>
+      <hr></hr>
+      <div className={styles.filterColor}>
+        <span className={styles.red}></span>
+        <p>Red</p>
+      </div>
+      <div className={styles.filterColor}>
+        <span className={styles.black}></span>
+        <p>Black</p>
+      </div>
+      <div className={styles.filterColor}>
+        <span className={styles.yellow}></span>
+        <p>Yellow</p>
+      </div>
+      <div className={styles.filterColor}>
+        <span className={styles.blue}></span>
+        <p>Blue</p>
+      </div>
+      <div className={styles.filterColor}>
+        <span className={styles.pink}></span>
+        <p>Pink</p>
+      </div>
+      <div className={styles.filterColor}>
+        <span className={styles.green}></span>
+        <p>Green</p>
+      </div>
+    </div>
+  );
+};
+
+export default FilterColor;
