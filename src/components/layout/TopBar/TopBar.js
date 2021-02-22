b7a15b8199302627aa881232289322d8691df909
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-02-22 15:58:46 +0100

    Add dropdown menu to the TopBar lists

--- src/components/layout/TopBar/TopBar.js
+++ src/components/layout/TopBar/TopBar.js
@@ -17,16 +17,46 @@ const TopBar = () => (
               <a href='/#'>
                 USD <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
               </a>
+              <ul className={styles.list}>
+                <li>
+                  <a href='/#'>USD</a>
+                </li>
+                <li>
+                  <a href='/#'>PLN</a>
+                </li>
+                <li>
+                  <a href='/#'>EUR</a>
+                </li>
+              </ul>
             </li>
             <li>
               <a href='/#'>
                 English <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
               </a>
+              <ul className={styles.list}>
+                <li>
+                  <a href='/#'>English</a>
+                </li>
+                <li>
+                  <a href='/#'>Polish</a>
+                </li>
+              </ul>
             </li>
             <li>
               <a href='/#'>
                 Help <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
               </a>
+              <ul className={styles.list}>
+                <li>
+                  <a href='/#'>Payment_options</a>
+                </li>
+                <li>
+                  <a href='/#'>Delivery</a>
+                </li>
+                <li>
+                  <a href='/#'>Shop_list</a>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
