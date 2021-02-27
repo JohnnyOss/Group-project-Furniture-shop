40dfd3f240e8514703e6633496a09563abdb3168
unknown <kowalczyk.przemek@wp.pl>
2021-02-27 19:06:06 +0100

    Add stage before commit

--- src/components/features/QuickView/QuickView.js
+++ src/components/features/QuickView/QuickView.js
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './QuickView.module.scss';
 
 const QuickView = ({}) => {
-    return (
-        <div className={styles.component}>
-          <div>
-             <h1>Quick View</h1>
-             <p>testing!</p> 
-          </div>
-        </div>
-    );
+  return (
+    <div className={styles.component}>
+      <div>
+        <h1>Quick View</h1>
+        <p>testing!</p> 
+      </div>
+    </div>
+  );
 };
 
 export default QuickView;
