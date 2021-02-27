b391f6c2d332f85e2df2ea37053abc572151b0f8
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-02-27 02:01:46 +0100

    Change idea

--- src/components/features/SortBy/SortBy.js
+++ src/components/features/SortBy/SortBy.js
@@ -1,12 +1,19 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './SortBy.module.scss';
 
-const SortBy = () => {
+const SortBy = ({ products, category }) => {
+  const sortingProducts = e => {};
+  let sorted = products.filter(item => item.category === category);
+  // if(e.currentTarget === 'Price: Lowest first') {
+  //   .sort(())
+
+  // }
+
   return (
     <div className={styles.filtersContainer}>
       <div className={styles.filter}>
-        <select>
+        <select onChange={e => sortingProducts(e)}>
           <option>Price: Lowest first</option>
           <option>Price: Highest first</option>
           <option>Name: A-Z</option>
@@ -18,8 +25,9 @@ const SortBy = () => {
   );
 };
 
-// SortBy.propTypes = {
-
-// };
+SortBy.propTypes = {
+  products: PropTypes.array,
+  category: PropTypes.string,
+};
 
 export default SortBy;
